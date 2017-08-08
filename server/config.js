697edae626ae0af130c7c8ee9be20952ef0daad2
52lions06 <mckelvey.william@yahoo.com>
2017-08-08 14:15:13 -0400

    changed the .env path and updated the port

--- server/config.js
+++ server/config.js
@@ -6,11 +6,11 @@ require('dotenv').config({path: __dirname + '/.env'}) // converts .env file into
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                       'mongodb://localhost/notehero';
-                      console.log(process.env.DATABASE_URL);
+                      // console.log('process.env.DATABASE_URL);
 exports.TEST_DATABASE_URL = (
   process.env.TEST_DATABASE_URL ||
  'mongodb://localhost/test-notehero');
-exports.PORT = process.env.PORT || 8080;
+exports.PORT = process.env.PORT || 3001;
 
 
 // mongodump -h ds135912.mlab.com:35912 -d notehero -u 'dev' -p '1' -o ~/
