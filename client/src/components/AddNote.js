7abd6d5c5ea658b8aba51be646dab29a70560652
joncancode <jc.haines19@gmail.com>
2017-08-08 16:26:56 -0400

    add button working, but needs to connect for both text boxes

--- client/src/components/AddNote.js
+++ client/src/components/AddNote.js
@@ -4,27 +4,28 @@ import { connect } from 'react-redux';
 import './AddNote.css';
 
 export class AddNote extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            //editing: false
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      //editing: false
+    };
 
-        this.submitPress = this.submitPress.bind(this);
-    }
-
+    this.submitPress = this.submitPress.bind(this);
+  }
 
   submitPress(event) {
     event.preventDefault();
-    console.log(this.textInput.value.trim())
-    const text = this.textInput.value.trim()
+    console.log(this.textInput.value.trim());
+    console.log('.');
+    const text = this.textInput.value.trim();
     if (text && this.props.onAdd) {
       this.props.onAdd(this.textInput.value);
     }
 
+    this.textInput.value = '';
+
     // console.log(textInput)
     // this.textInput.value = '';
-
 
     // this.props.dispatch(addNote(text))
   }
@@ -35,25 +36,21 @@ export class AddNote extends React.Component {
         <form onSubmit={this.props.onSubmit} aria-label="enter form">
           <input
             type="text"
-            ref={input => this.textInput = input}
+            ref={input => (this.textInput = input)}
             name="addInput"
             placeholder="vocabulary word"
-            value={this.props.numberInput}
             onChange={this.props.onChange}
             aria-label="enter a vocab word"
             required
           />
-          <button
-            type="submit"
-            value="Submit"
-            onClick={this.submitPress}
-          >
+          <button type="submit" value="Submit" onClick={this.submitPress}>
             ADD NOTE
           </button>
           <br />
           <textarea
             rows="2"
             cols="25"
+            ref={textArea => (this.textInput = textArea)}
             aria-label="enter definition"
             placeholder="enter definition"
             required
