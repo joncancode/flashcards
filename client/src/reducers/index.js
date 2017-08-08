1f5752ac7d51b8b499186c476bc7174e02f75a0f
joncancode <jc.haines19@gmail.com>
2017-08-07 21:07:49 -0400

    fixed card text

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,10 +2,10 @@ import { ADD_NOTE, TOGGLE_NOTE } from '../actions';
 
 const initialState = {
     cards: 
-        [{words: "word: Closure",
-        definition: "definition: an inner function that has access to the outer (enclosing) function's variables—scope chain"},
-        {words: "word: Hoisting",
-        definition: "definition: JavaScript interpreter's action of moving all variable and function declarations to the top of the current scope"}],
+        [{words: "Closure",
+        definition: "an inner function that has access to the outer (enclosing) function's variables—scope chain"},
+        {words: "Hoisting",
+        definition: "JavaScript interpreter's action of moving all variable and function declarations to the top of the current scope"}],
     }
 
 
