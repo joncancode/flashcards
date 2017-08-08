502c9b7a66ee0ff7c8da3b0cf90ec33bfcb9cf6c
joncancode <jc.haines19@gmail.com>
2017-08-08 16:31:58 -0400

    fixed both input boxes

--- client/src/components/AddNote.js
+++ client/src/components/AddNote.js
@@ -15,14 +15,15 @@ export class AddNote extends React.Component {
 
   submitPress(event) {
     event.preventDefault();
-    console.log(this.textInput.value.trim());
-    console.log('.');
+    console.log(this.textInput.value.trim(), 'textarea');
+    console.log(this.textArea.value.trim(), 'inputbox');
     const text = this.textInput.value.trim();
     if (text && this.props.onAdd) {
       this.props.onAdd(this.textInput.value);
     }
 
     this.textInput.value = '';
+    this.textArea.value = '';
 
     // console.log(textInput)
     // this.textInput.value = '';
@@ -50,7 +51,7 @@ export class AddNote extends React.Component {
           <textarea
             rows="2"
             cols="25"
-            ref={textArea => (this.textInput = textArea)}
+            ref={textArea => (this.textArea = textArea)}
             aria-label="enter definition"
             placeholder="enter definition"
             required
