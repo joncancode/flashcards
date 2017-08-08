834d49dee7bc2a8f42ab77ff0f73fe48ff2851b4
52lions06 <mckelvey.william@yahoo.com>
2017-08-08 14:29:04 -0400

    activate the post for /api/notes/name

--- server/index.js
+++ server/index.js
@@ -90,115 +90,122 @@ app.post('/api/notes/:name', (req, res) => { //this is the pose when we are clic
 
   Note
     .create({
-      word:
+      word: req.body.word,
+      definition: req.body.definition
     })
-//   console.log('get all is happening');
-//   Note
-//     .find()
-//     .then(Notes => {
-//       console.log(Notes);
-//       res.status(200).json(Notes);
-//     })
-//     .catch(err => {
-//       console.log('testing');
-//       res.status(500).json({ message: 'Internal error from GET' });
-//     });
-});
-
-app.post('/beers', (req, res) => {
-  const requiredFields = ['name', 'style', 'abv', 'description', 'reviews', 'brewery', 'ibu', ];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`;
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-
-  Beer
-    .create({
-      name: req.body.name,
-      abv: req.body.abv,
-      style: req.body.style,
-      reviews: req.body.reviews,
-      brewery: req.body.brewery,
-      ibu: req.body.ibu,
-      description: req.body.description})
     .then(
-      beer => res.status(201).json(beer.apiRepr()))
+      note => res.status(201).json(note.apiRepr()))
     .catch(err => {
-      console.error(err);
+      console.log(err);
       res.status(500).json({message: 'Internal server error'});
     });
+
+  console.log('get all is happening');
+  Note
+    .find()
+    .then(Notes => {
+      console.log(Notes);
+      res.status(200).json(Notes);
+    })
+    .catch(err => {
+      console.log('testing');
+      res.status(500).json({ message: 'Internal error from GET' });
+    });
 });
+// app.post('/beers', (req, res) => {
+//   const requiredFields = ['name', 'style', 'abv', 'description', 'reviews', 'brewery', 'ibu', ];
+//   for (let i=0; i<requiredFields.length; i++) {
+//     const field = requiredFields[i];
+//     if (!(field in req.body)) {
+//       const message = `Missing \`${field}\` in request body`;
+//       console.error(message);
+//       return res.status(400).send(message);
+//     }
+//   }
+
+//   Beer
+//     .create({
+//       name: req.body.name,
+//       abv: req.body.abv,
+//       style: req.body.style,
+//       reviews: req.body.reviews,
+//       brewery: req.body.brewery,
+//       ibu: req.body.ibu,
+//       description: req.body.description})
+//     .then(
+//       beer => res.status(201).json(beer.apiRepr()))
+//     .catch(err => {
+//       console.error(err);
+//       res.status(500).json({message: 'Internal server error'});
+//     });
+// });
 
 
 
 
-app.post('/api/notes/:name/:words', (req, res) => { // this is the post for when we click on the card and we are 
-//   console.log('get all is happening');
+// app.post('/api/notes/:name/:words', (req, res) => { // this is the post for when we click on the card and we are 
+// //   console.log('get all is happening');
+// //   Note
+// //     .find()
+// //     .then(Notes => {
+// //       console.log(Notes);
+// //       res.status(200).json(Notes);
+// //     })
+// //     .catch(err => {
+// //       console.log('testing');
+// //       res.status(500).json({ message: 'Internal error from GET' });
+// //     });
+// });
+
+// app.put('/api/notes/:name/:words', (req, res) => { });
+
+// app.put('/posts/:id', (req, res) => {
+//   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+//     res.status(400).json({
+//       error: 'Request path id and request body id values must match'
+//     });
+//   }
+
+//   const updated = {};
+//   const updateableFields = ['words', 'definition'];
+//   updateableFields.forEach(field => {
+//     if (field in req.body) {
+//       updated[field] = req.body[field];
+//     }
+//   });
+
+//   BlogPost
+//     .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
+//     .exec()
+//     .then(updatedPost => res.status(201).json(updatedPost.apiRepr()))
+//     .catch(err => res.status(500).json({ message: 'Something went wrong' }));
+// });
+
+// app.delete('/api/notes/:name/', (req, res) => {
 //   Note
-//     .find()
-//     .then(Notes => {
-//       console.log(Notes);
-//       res.status(200).json(Notes);
+//     .findByIdAndRemove(req.params.id)
+//     .then(result => {
+//       console.log(result);
+//       res.status(204).send({ message: 'Deleted' });
 //     })
 //     .catch(err => {
-//       console.log('testing');
-//       res.status(500).json({ message: 'Internal error from GET' });
+//       console.log(err);
+//       res.status(500).send({ message: 'Error from Delete' });
 //     });
-});
+// });
 
-app.put('/api/notes/:name/:words', (req, res) => { });
-
-app.put('/posts/:id', (req, res) => {
-  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-    res.status(400).json({
-      error: 'Request path id and request body id values must match'
-    });
-  }
-
-  const updated = {};
-  const updateableFields = ['words', 'definition'];
-  updateableFields.forEach(field => {
-    if (field in req.body) {
-      updated[field] = req.body[field];
-    }
-  });
-
-  BlogPost
-    .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
-    .exec()
-    .then(updatedPost => res.status(201).json(updatedPost.apiRepr()))
-    .catch(err => res.status(500).json({ message: 'Something went wrong' }));
-});
-
-app.delete('/api/notes/:name/', (req, res) => {
-  Note
-    .findByIdAndRemove(req.params.id)
-    .then(result => {
-      console.log(result);
-      res.status(204).send({ message: 'Deleted' });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).send({ message: 'Error from Delete' });
-    });
-});
-
-app.delete('/api/notes/:name/:words', (req, res) => {
-  Note
-    .findByIdAndRemove(req.params.id)
-    .then(result => {
-      console.log(result);
-      res.status(204).send({ message: 'Deleted' });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).send({ message: 'Error from Delete' });
-    });
-});
+// app.delete('/api/notes/:name/:words', (req, res) => {
+//   Note
+//     .findByIdAndRemove(req.params.id)
+//     .then(result => {
+//       console.log(result);
+//       res.status(204).send({ message: 'Deleted' });
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).send({ message: 'Error from Delete' });
+//     });
+// });
 
 
 
