890b4094b24573c84c9fe00d246f5df8f2d3b33b
joncancode <jc.haines19@gmail.com>
2017-08-09 13:57:30 -0400

    trying to add CORS

--- server/index.js
+++ server/index.js
@@ -18,6 +18,12 @@ app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  //res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'GET', 'DELETE');
+  next();
+});
 
 // API endpoints go here!
 app.get('/api/notes', (req, res) => {
