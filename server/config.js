75dcb4fa2317fa203aa9998521330ff1b6d3270f
joncancode <jc.haines19@gmail.com>
2017-08-10 14:35:40 -0400

    deploys successfully in heroku

--- server/config.js
+++ server/config.js
@@ -1,17 +1,14 @@
-"use strict"
-
+'use strict';
 
 require('dotenv').config(); // converts .env file into an object: process.env
 
-exports.DATABASE_URL= 'mongodb://dev:1@ds135912.mlab.com:35912/notehero'
 
-// exports.DATABASE_URL = process.env.DATABASE_URL ||
-//                        global.DATABASE_URL ||
-//                       'mongodb://localhost/notehero';
-//                       // console.log('process.env.DATABASE_URL);
-// exports.TEST_DATABASE_URL = (
-//   process.env.TEST_DATABASE_URL ||
-//  'mongodb://localhost/test-notehero');
+exports.DATABASE_URL =
+  process.env.DATABASE_URL ||
+  global.DATABASE_URL ||
+  'mongodb://localhost/notehero';
+exports.TEST_DATABASE_URL =
+  process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-notehero';
 exports.PORT = process.env.PORT || 3001;
 
 // console.log('mLab', exports.DATABASE_URL);
