284f4bea8fb05da0241caef8817e1bdd626fb6c6
joncancode <jc.haines19@gmail.com>
2017-08-10 14:18:11 -0400

    configuring the config so that the heroku works properly

--- server/config.js
+++ server/config.js
@@ -3,16 +3,18 @@
 
 require('dotenv').config(); // converts .env file into an object: process.env
 
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                      'mongodb://localhost/notehero';
-                      // console.log('process.env.DATABASE_URL);
-exports.TEST_DATABASE_URL = (
-  process.env.TEST_DATABASE_URL ||
- 'mongodb://localhost/test-notehero');
+export.DATABASE_URL= 'mongodb://dev:1@ds135912.mlab.com:35912/notehero'
+
+// exports.DATABASE_URL = process.env.DATABASE_URL ||
+//                        global.DATABASE_URL ||
+//                       'mongodb://localhost/notehero';
+//                       // console.log('process.env.DATABASE_URL);
+// exports.TEST_DATABASE_URL = (
+//   process.env.TEST_DATABASE_URL ||
+//  'mongodb://localhost/test-notehero');
 exports.PORT = process.env.PORT || 3001;
 
-console.log('mLab', exports.DATABASE_URL);
+// console.log('mLab', exports.DATABASE_URL);
 
 //equire('dotenv').config({path: __dirname + '/.env'})
 // mongodump -h ds135912.mlab.com:35912 -d notehero -u 'dev' -p '1' -o ~/
