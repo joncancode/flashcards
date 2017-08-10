589acd4465bda1a61819a631aeb0f9e4693e7e46
52lions06 <mckelvey.william@yahoo.com>
2017-08-10 10:56:04 -0400

    connected mLab by pushing in .env

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 "use strict"
 
 
-require('dotenv').config({path: __dirname + '/.env'}) // converts .env file into an object: process.env
+require('dotenv').config(); // converts .env file into an object: process.env
 
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
@@ -12,5 +12,7 @@ exports.TEST_DATABASE_URL = (
  'mongodb://localhost/test-notehero');
 exports.PORT = process.env.PORT || 3001;
 
+console.log('mLab', exports.DATABASE_URL);
 
+//equire('dotenv').config({path: __dirname + '/.env'})
 // mongodump -h ds135912.mlab.com:35912 -d notehero -u 'dev' -p '1' -o ~/
