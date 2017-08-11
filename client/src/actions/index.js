185cae7099d512c75533ec7d348ac5a5c7c26b59
joncancode <jc.haines19@gmail.com>
2017-08-11 00:00:01 -0400

    need to figure out why delete request works on local but not to heroku

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -72,8 +72,8 @@ export const postNotes = newNote => {
 export const deleteNote = id => {
   return dispatch => {
     //we are returning another function for to us thunk so we can connect to the ajax call
-    //fetch('https://notehero.herokuapp.com/api/notes', {
-    fetch('http://localhost:8080/api/notes', {
+    fetch('notehero.herokuapp.com/api/notes', {
+    //fetch('http://localhost:8080/api/notes', {
       method: 'DELETE',
       headers: {
         Accept: 'application/json',
