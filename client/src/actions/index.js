b054155598cedf791ece4340803e89c088846c3b
joncancode <jc.haines19@gmail.com>
2017-08-12 18:34:20 -0400

    cleaning up unnec comments

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -19,8 +19,6 @@ export const checkOff = id => ({
   id
 });
 
-//async action
-
 //async action -- get
 export const fetchNotes = () => {
   return dispatch => {
@@ -55,20 +53,7 @@ export const postNotes = newNote => {
   };
 };
 
-// // asyn action -- put
-// export const postNotes = (newNote) =>{
-//     return (dispatch) => { //we are returning another function for to us thunk so we can connect to the ajax call
-//       fetch('http://localhost:8080/api/notes' , {
-//         method: 'POST',
-//         data: JSON.stringify({newNote})
-//         }) //redux-thunk gives us access to the dispatch -- from our local host (connecting to the server)
-//   .then(response =>  response.json())
-//   .then(json => dispatch(addNote(json)))   //this is where you pass in a dispatched action so that you can recieve the information -- same as | console.log('parsed json', json)
-//   .catch(ex => console.log('parsing failed', ex))
-//     }
-// }
-
-// asyn action -- delete
+// async action -- delete
 export const deleteNote = id => {
   return dispatch => {
     //we are returning another function for to us thunk so we can connect to the ajax call
